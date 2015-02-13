c7b89da26056987686bb14f59986cdf6fc471450
Tyler <tyler.r@sympatico.ca>
2015-02-12 20:51:44 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-	render text: "what up mothafuckas"
+	render text: "Hello friends, how are you?"
   end
 
 end
